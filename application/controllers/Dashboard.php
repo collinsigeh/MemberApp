44bc53201fa0d905ccf03249881bceeb19be15ad
Collins Igeh <igehac@gmail.com>
2020-09-21 09:01:58 +0100

    including logic to the Dashboard index method

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,13 +5,18 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$data = array(
-			'page_title' => 'Dashboard'
-		);
+		if($this->session->userlogged_in == '*#loggedin@Yes')
+		{
+			$data = array(
+				'page_title' => 'Dashboard'
+			);
+	
+			$this->load->view('templates/header', $data);
+			$this->load->view('dashboard_view');
+			$this->load->view('templates/footer');
+		}
 
-		$this->load->view('templates/header', $data);
-		$this->load->view('dashboard_view');
-		$this->load->view('templates/footer');
+		redirect(base_url().'dashboard/login/');
 	}
 
 	public function register()
