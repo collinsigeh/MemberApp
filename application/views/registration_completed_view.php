e0c74a469d8034280ee26e02b97fa64b13301daa
Collins Igeh <igehac@gmail.com>
2020-09-25 18:07:02 +0100

    created registration_completed_view

---
+++ application/views/registration_completed_view.php
@@ -0,0 +1,18 @@
+<!-- Page Content -->
+<div class="container">
+  <div class="page">
+    <div class="row">
+      <div class="col-md-10 offset-md-1">
+        <div class="main-content">
+          <!-- Main content -->
+          <?php 
+            include('inc/action_message.php');
+          ?>
+          
+          <div class="text-center">
+            <a href="<?php echo base_url(); ?>" class="custom_buttom">Back to Homepage</a>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
