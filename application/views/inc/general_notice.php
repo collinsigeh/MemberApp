5d5e1cb36f97fecad26bf281658db3d6102a7fd2
Collins Igeh <igehac@gmail.com>
2020-10-02 15:34:37 +0100

    started the cleanup of dashboard - the general notifications

--- application/views/inc/general_notice.php
+++ application/views/inc/general_notice.php
@@ -1,7 +1,22 @@
 <div class="notice">
-    <div class="alert alert-info">
-      <img src="<?php echo base_url(); ?>assets/img/icon_images/notice.png" class="notice-icon" ><b>Notice:</b> Your account is Pending Approval.          
-    </div>
+  <?php
+    if($this->session->status == 'Pending Approval')
+    {
+      ?>
+      <div class="alert alert-info">
+        <img src="<?php echo base_url(); ?>assets/img/icon_images/notice.png" class="notice-icon" ><b>Notice:</b> Your account is pending approval.          
+      </div>
+      <?php
+    }
+    if($this->session->status == 'Suspended')
+    {
+      ?>
+      <div class="alert alert-danger">
+        <img src="<?php echo base_url(); ?>assets/img/icon_images/notice.png" class="notice-icon" ><b>Notice:</b> Your account is suspended.          
+      </div>
+      <?php
+    }
+  ?>
     <div class="alert alert-info">
       <img src="<?php echo base_url(); ?>assets/img/icon_images/notice.png" class="notice-icon" ><b>Notice:</b> Your subscription has expired.          
     </div>
