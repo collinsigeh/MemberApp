db4ba17fa90883be1f28faa76c2fbff7f5d75177
Collins Igeh <igehac@gmail.com>
2020-10-18 20:53:24 +0100

    created the subscriptons controller

---
+++ application/controllers/Subscriptons.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Subscriptions extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->model('user_model');
+		$this->load->model('payment_processor_model');
+		$this->load->model('order_model');
+		$this->load->model('product_model');
+		$this->load->model('subscription_product_model');
+		$this->load->model('member_subscription_model');
+        $this->load->model('payment_model');
+        $this->load->model('setting_model');
+
+		$this->load->library('form_validation');
+		$this->load->library('email');
+    }
+
+    /*
+    * Check for due subscriptions and notifies the member
+    */
+    public function due_for_renewals()
+    {
+
+    }
+}
