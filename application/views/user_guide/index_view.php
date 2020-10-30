497293d80ffb6f89f59c1a189d4fb7a4552c7235
Collins Igeh <igehac@gmail.com>
2020-10-30 16:18:38 +0100

    added a second userguide section

--- application/views/user_guide/index_view.php
+++ application/views/user_guide/index_view.php
@@ -18,7 +18,7 @@
             <div class="section-links">
                 <ul>
                     <li><a href="#how-to-verify-approve-new-member-account">How to verify / approve new member account</a></li>
-                    <li><a href="#">How to grant subscription to a member</a></li>
+                    <li><a href="#how-to-grant-subscription-to-a-member">How to grant subscription to a member</a></li>
                 </ul>
             </div>
 
@@ -33,6 +33,21 @@
                     <p>Scroll to the <b>Account setting</b> section, select the <b>Account status</b> of choice.</p>
                     <img src="<?php echo base_url().'assets/img/user_guide/image_3.png'; ?>" alt="photo_ref">
                     <p>Scroll down and click on the <b>Save</b> button.</p>
+                </div>
+            </div>
+
+            <div class="section-item">
+                <h4 class="sub-heading" id="how-to-grant-subscription-to-a-member">How to grant subscription to a member</h4>
+                <div class="answer">
+                    <p>Ensure you are logged in (<i>of cause you have to be an admin</i>).</p>
+                    <p>Click on the <b>User Accounts</b> link.</p>
+                    <img src="<?php echo base_url().'assets/img/user_guide/image_1.png'; ?>" alt="photo_ref">
+                    <p>Look for and click on the user to verify/confirm.</p>
+                    <img src="<?php echo base_url().'assets/img/user_guide/image_2.png'; ?>" alt="photo_ref">
+                    <p>Scroll to the <b>Account setting</b> section, click on the <b>Add new subscription</b> button.</p>
+                    <img src="<?php echo base_url().'assets/img/user_guide/image_4.png'; ?>" alt="photo_ref">
+                    <p>Fill out the form that pops up and click on the <b>Submit</b> (<i>button as shown below</i>).</p>
+                    <img src="<?php echo base_url().'assets/img/user_guide/image_5.png'; ?>" alt="photo_ref">
                 </div>
             </div>
           </div>
